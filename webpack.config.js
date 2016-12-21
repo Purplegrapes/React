f971385b9d1d36ca0bdf9b71b31d2db10e0ef31b
张琼 <zhangqiong@zhangqiongdeMacBook-Pro.local>
2016-12-21 09:58:30 +0800

    first update

---
+++ webpack.config.js
@@ -0,0 +1,22 @@
+/**
+ * Created by zhangqiong on 16/12/20.
+ */
+/**
+ * Created by zhangqiong on 16/12/15.
+ */
+const webpack = require('webpack');
+
+module.exports = {
+  entry: './entry.js',
+  output: {
+    path: __dirname,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {test: /\.less$/, loader: 'style!css?sourceMap=true!less?sourceMap=true'},
+      {test: /\.js$/, exclude: /node_modules/, loader: "babel-loader"}
+    ]
+  },
+  devtool: "inline-source-map"
+};
