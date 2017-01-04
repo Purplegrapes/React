5dd6f8e02961c71c585c430b5bcc7ee1ba3c801d
张琼 <zhangqiong@zhangqiongdeMacBook-Pro.local>
2017-01-04 13:35:33 +0800

    update localStorage

--- middlewares/LocalStorage.js
+++ middlewares/LocalStorage.js
@@ -1,30 +1,50 @@
 /**
  * Created by zhangqiong on 17/1/3.
  */
-import { ADD_TODO, FETCH_TODOS, GET_TODOS, DEL_TODO, VisibilityFilters } from '../actions/action';
+import {
+  ADD_TODO,
+  FETCH_TODOS,
+  TOGGLE_ALL,
+  CLEAR_COMPLETE,
+  GET_TODOS,
+  DEL_TODO,
+  COMPLETE_TODO,
+} from '../actions/action';
 
 export const setLocalStroage = ({ getState }) => next => (action) => {
-  if (action.type === ADD_TODO) {
-    const result = next(action);
-    const todos = getState().todos;
-    const nowdata = todos[todos.length - 1];
-    console.log(nowdata);
-    const localdata = JSON.stringify(nowdata);
-    const key = nowdata.id;
-    console.log(todos[todos.length - 1].id);
-    localStorage.setItem(key, localdata);
-    return result;
+  switch (action.type) {
+
+    case DEL_TODO: {
+      const result = next(action);
+      localStorage.removeItem(action.id);
+      return result;
+    }
+    case CLEAR_COMPLETE: {
+      const result = next(action);
+      console.log(action);
+      for (let i = 0; i < localStorage.length; i++) {
+        const data = JSON.parse(localStorage.getItem(localStorage.key(i)));
+        if (data.completed) {
+          localStorage.removeItem(data.id);
+        }
+      }
+      return result;
+    }
+    default: {
+      const result = next(action);
+      const nowdata = getState().todos;
+      for (const todo of nowdata) {
+        const localdata = JSON.stringify(todo);
+        localStorage.setItem(todo.id, localdata);
+      }
+      return result;
+    }
   }
-  else if (action.type === DEL_TODO) {
-    localStorage.removeItem(action.id);
-  }
-  return next(action);
 }
 export const getLocalStorage = () => next => (action) => {
   if (action.isLocal) {
     for (let i = 0; i < localStorage.length; i++) {
       const data = JSON.parse(localStorage.getItem(localStorage.key(i)));
-      console.log(data);
       const todo = { data, type: GET_TODOS };
       next(todo);
     }
